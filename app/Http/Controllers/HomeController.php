5861ec85a0015650585633c15c59d73b7b3960af
Muhammad <talhaishtiaq944@gmail.com>
2020-04-29 02:03:03 +0500

    Laravel acl

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Gate;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        if(!Gate::allows('isAdmin')){
+            abort(404,'Sorry, you are nout authorized');
+        }
+        return view('home');
+    }
+}
